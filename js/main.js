950ccf92d25cda98022c4aa9f308e749249a1932
FelixSchlatter <web@schwizerschlatter>
2017-05-08 09:00:12 +0200

    nklgg

--- js/main.js
+++ js/main.js
@@ -6,6 +6,24 @@ $('#container').click(function() {
 });
 
 
+var fullheight = $(window).height();
+$('.fullheight').height(fullheight);
+
+var resizeTimer,
+$window = $(window);
+function imageresize() {
+    var fullheight = $(window).height();
+    $('.fullheight').height(fullheight);
+    
+    if ($window.width() < 780) {}
+}
+imageresize();
+$window.resize(function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(imageresize, 100);
+});
+
+
 
 
 }); // ende doc ready
